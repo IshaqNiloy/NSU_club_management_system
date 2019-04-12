c3f9e6d361f635acfc2b4764e50a1b31efc6198a
IshaqNiloy <ishaqniloy1996@gmail.com>
2019-04-13 02:41:54 +0600

    Add files via upload

---
+++ finance_club_update.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Finance Club</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel ="stylesheet" href="css_files/index.css">
+  <script src="js/jquery-2.1.3.min.js"></script>
+  <script src="js/cycle2.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel ="stylesheet" href="css_files/index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Gothic+A1|Inconsolata|Roboto|Montserrat|Abril+Fatface" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="nevigation.html">NSU Club Management</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#" target="_blank">Search</a>
+      </li>
+      <li class= "nav-item" id="nav-log-out">
+        <a class="nav-link" href="index.php">Log out</a>
+      </li> 
+    </ul>
+  </div>  
+</nav>
+<br>
+
+<div class="container">
+  <h1 style="color: #000; margin-bottom: 20px">Welcome To Finance Club</h1>
+  <div class="row">
+    
+    </div>
+</div>
+<div class="col-sm-12"><p class="footer" style="color:#000; margin-top: 35px">&copy Ali's Corporation. All rights reserved. Developed by Mahadi Hasan Sohan, Ishaq Ali and Md. Sabbir Hossain.</p></div>
+</body>
+</html>
