d4b3b24cea38392ddce97243f2cb925571bc2da6
IshaqNiloy <ishaqniloy1996@gmail.com>
2019-04-13 02:43:49 +0600

    Add files via upload

---
+++ model_un_club_form.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Model UN Club</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel ="stylesheet" href="css_files/index.css">
+  <script src="js/jquery-2.1.3.min.js"></script>
+  <script src="js/cycle2.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel ="stylesheet" href="css_files/index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Gothic+A1|Inconsolata|Roboto|Montserrat|Abril+Fatface" rel="stylesheet">
+</head>
+<body style="background-image: linear-gradient(to bottom right, #ff0844 0%, #ffb199 100%)">
+
+<nav class="navbar navbar-inverse navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="nevigation.html">NSU Club Management</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#" target="_blank">Search</a>
+      </li>
+      <li class= "nav-item" id="nav-log-out">
+        <a class="nav-link" href="#">Log out</a>
+      </li> 
+    </ul>
+  </div>  
+</nav>
+<br>
+
+<div class="container">
+  <h1 style="color: #fff;">Model UN Club Form</h1>
+  <div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6" style="background-color: rgb(0,0,0,0.5)">
+    <form action="model_un_club_savedata.php" method="post">
+    <div class="row">
+        <div class="col-sm-12">
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px; margin-top: 15px">Name:</p>
+          <input type ="text" class="form-control"  name = "name"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">ID:</p>
+          <input type = "number" class="form-control" name="ID"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Phone:</p> 
+          <input type = "number" class="form-control" name="phone"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Blood Group:</p> 
+          <input type = "text" class="form-control" name="blood_group"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Department:</p> 
+          <input type = "text" class="form-control" name="department"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Address:</p> 
+          <input type = "text" class="form-control" name="address"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Interested In:</p> 
+          <input type = "text" class="form-control" name="interested_in"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Skills:</p> 
+          <input type = "text" class="form-control" name="skills"><br>
+          <p style="font-weight: bold; color: #fff;letter-spacing: 1px">Hobby:</p> 
+          <input type = "text" class="form-control" name="hobby"><br>
+        </div>
+        <div class="col-sm-12" style="margin-bottom: 0px;padding-bottom: 0px"> 
+          <button type="submit" class="btn btn-default" id="btn-submit" style="margin-left: 83%; float: left">Submit</button><br><br>
+        </div>
+        </div>
+        </div>
+        </form>
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+</div>
+<div class="col-sm-12"><p class="footer">&copy Ali's Corporation. All rights reserved. Developed by Mahadi Hasan Sohan, Ishaq Ali and Md. Sabbir Hossain</p></div>
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Navigation page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel ="stylesheet" href="css_files/index.css">
+  <script src="js/jquery-2.1.3.min.js"></script>
+  <script src="js/cycle2.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel ="stylesheet" href="css_files/index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Gothic+A1|Inconsolata|Roboto|Montserrat|Abril+Fatface" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-expand-md bg-dark navbar-dark" style="background-image: linear-gradient(to bottom right, #dc2430 0%, #7b4397 100%);">
+  <a class="navbar-brand" href="#">NSU Club Management</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link" href="#" target="_blank">Search</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Log out</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+<br>
+<div class="container-fluid">
+<h1 style="margin-bottom: 2%">Welcome To NSU Club Management</h1>
+  <div class="card-columns">
+    <a href="finance_club.php" target="_blank">
+    <div style="background-image: linear-gradient(to right, #ed6ea0 0%, #ec8c69 100%);" class="card">
+    <img src="images/finance.png" alt="card image" style= "">
+      <div class="card-body text-center">
+        <p class="card-text"><strong>Finance Club</strong></p>
+      </div>
+    </div>
+    </a>
+    <a href="human_resources_club_form.php" target="_blank">
+    <div style="background-image: linear-gradient(to bottom right, #30cfd0 0%, #330867 100%);" class="card">
+    <img src="images/human.png" alt="card image">
+      <div class="card-body text-center">
+        <p class="card-text"><strong>Human Resources Club</strong></p>
+      </div>
+    </div>
+    </a>
+    <a href="international_business_club_form.php" target="_blank">
+      <div style = "background-image: linear-gradient(to bottom right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%);" class="card">
+        <img src="images/business.png" alt="form image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>International Business Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="marketing_club_form.php" target="_blank">
+    <div style="background-image: linear-gradient(to bottom right, #ff0844 0%, #ffb199 100%);" class="card">
+    <img src="images/marketing.png" alt="card image" style="width: 70%; margin-left: 17%">
+      <div class="card-body text-center">
+        <p class="card-text"><strong>Marketing Club</strong></p>
+      </div>
+    </div>
+    </a>
+    <a href="mba_club_form.php" target="_blank">
+    <div style="background-image: linear-gradient(to bottom right, #16a085 0%, #f4d03f 100%);"class="card">
+    <img src="images/nba.png" alt="card image">
+      <div class="card-body text-center">
+        <p class="card-text"><strong>MBA Club</strong></p>
+      </div>
+    </div>
+    </a>
+    <a href="young_economists_forum_form.php" target="_blank">
+    <div style="background-image: linear-gradient(to bottom right, #243949 0%, #517fa4 100%);"class="card">
+    <img src="images/economics.png" alt="card image">
+      <div class="card-body text-center">
+        <p class="card-text"><strong>Young Economists Forum</strong></p>
+      </div>
+    </div>
+    </a>
+    <a href="young_entrepreneurs_society_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #d7d2cc 0%, #304352 100%);"class="card">
+        <img src="images/entrepreneurs.png" alt="card image" style="width: 79%;; margin-left: 8%">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Young Entrepreneurs Society</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="communications_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #e14fad 0%, #f9d423 100%);"class="card">
+        <img src="images/communication.png" alt="card image" style="width: 35%; height: 40%; margin-left: 33%">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Communications Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="diversity_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #4da0b0 0%, #d39d38  100%)"class="card">
+        <img src="images/diversity.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Diversity Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="law_and_mooting_society_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #20E2D7 0%, #F9FEA5 100%);"class="card">
+        <img src="images/law.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Law and Mooting Society</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="model_un_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #2CD8D5 0%, #C5C1FF 56%, #FFBAC3 100%);"class="card">
+        <img src="images/model.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Model UN Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="earth_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #DFFFCD 0%, #90F9C4 48%, #39F3BB 100%);"class="card">
+        <img src="images/earth.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Earth Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="public_health_and_sciences_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #A8BFFF 0%, #884D80 100%);"class="card">
+        <img src="images/health.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Public Health & Sciences Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="pharmaceutical_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #c1c161 0%, #c1c161 0%, #d4d4b1 100%);"class="card">
+        <img src="images/pharmaceutical.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Pharmaceutical Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="bio_sci_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #d5dee7 0%, #ffafbd 0%, #c9ffbf 100%);"class="card">
+        <img src="images/bio_sci.png" alt="card image" style="width: 42%;">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Bio Sc. Club (Proposed)</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="art_and_photography_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #d5dee7 0%, #ffafbd 0%, #c9ffbf 100%);"class="card">
+        <img src="images/photography.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Art & Photography Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="computer_and_engineering_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #9be15d 0%, #00e3ae 100%);"class="card">
+        <img src="images/computer.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Computer & Engineering Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="athletics_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #b3ffab 0%, #12fff7 100%);"class="card">
+        <img src="images/athletics.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Athletics Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="cine_and_drama_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #d5dee7 0%, #ffafbd 0%, #c9ffbf 100%);"class="card">
+        <img src="images/cine.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Cine & Drama Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="debate_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #bdc2e8 0%, #bdc2e8 1%, #e6dee9 100%);"class="card">
+        <img src="images/debate.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Debate Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="ethics_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #f794a4 0%, #fdd6bd 100%);"class="card">
+        <img src="images/ethics.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Ethics Club</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="sangskritkik_sangathan_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #a8caba 0%, #5d4157 100%);"class="card">
+        <img src="images/culture.png" alt="card image" style="">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Sangskritkik Sangathan</strong></p>
+        </div>
+      </div>
+    </a>
+    <a href="social_services_club_form.php" target="_blank">
+      <div style="background-image: linear-gradient(to bottom right, #abecd6 0%, #fbed96 100%);"class="card">
+        <img src="images/social_services.png" alt="card image" style="width: 60%;">
+        <div class="card-body text-center">
+          <p class="card-text"><strong>Social Services Club</strong></p>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+<div class="footer" style="background-image: linear-gradient(to bottom right, #dc2430 0%, #7b4397 100%);"><p>&copy North South University. All rights reserved. Developed and maintained by<br></p><br>
+  <div class="row">
+    <div class="col-sm-4"><p><img src="images/sohan.jpg"><br>Mahadi Hassan Sohan</p></div>
+    <div class="col-sm-4"><p><img src="images/ishaq.jpg"><br>Ishaq Ali</p></div>
+    <div class="col-sm-4"><p><img src="images/sabbir.jpg"><br>Md. Sabbir Hossain</p></div>
+  </div>
+</div> 
+</div>
+</body>
+</html>
